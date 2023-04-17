5afb90de70ab745d4f56170b8335ff9860835bf2
praveen <praveenkk8693@gmail.com>
2023-04-17 16:11:29 +0530

    initial commit

---
+++ git-all-activities/src/main/java/com/asc/git/git_all_activities/App.java
@@ -0,0 +1,13 @@
+package com.asc.git.git_all_activities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ git-all-activities/src/test/java/com/asc/git/git_all_activities/AppTest.java
@@ -0,0 +1,20 @@
+package com.asc.git.git_all_activities;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
