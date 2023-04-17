9e3376a198defc1cfe2c76a0a2ac593859744049
praveen <praveenkk8693@gmail.com>
2023-04-17 17:58:18 +0530

    updated for eng

--- git-all-activities/src/main/java/com/asc/git/git_all_activities/App.java
+++ git-all-activities/src/main/java/com/asc/git/git_all_activities/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Automation Architect" );
+        System.out.println( "Automation Engineer" );
     }
 }
