35e2faf0ce8ad6fca0758fedfcbc83e6a183bdd6
praveenkk8693 <120715027+praveenkk8693@users.noreply.github.com>
2023-05-04 20:34:03 +0530

    Update App.java

--- git-all-activities/src/main/java/com/asc/git/git_all_activities/App.java
+++ git-all-activities/src/main/java/com/asc/git/git_all_activities/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Automation Engineer" );
+        System.out.println( "Automation Engineer Architect role 4May" );
     }
 }
